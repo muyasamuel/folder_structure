3179767bc556448fbd41ea416248ac283a59972a
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-28 15:18:26 +0300

    Introduce a method to get the file extension name for the icon

--- src/Folder.js
+++ src/Folder.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import Icon from './Icon';
 
-function Folder() {
+function Folder({files}) {
+
+    const findFileExtensionName = (fileName) => {
+        const arr = fileName.split('.');
+        return arr[arr.length - 1].toLowerCase();
+    }
+
+
   return (
     <div style={{display: 'flex' , alignItems: "center"}}>
+        {!files.isFolder  &&
+        <>
         <Icon name='js'   style={{ height: '19px', marginRight: '8px', flexShrink: 0 }} />
-        <p>Root </p>
+         <p>{files.name}</p> 
+        </>
+         }
+        
+       
     </div>
   )
 }
