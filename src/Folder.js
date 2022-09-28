2b9019b016ddcf6b263dc512b68c0567c5ea6e2b
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-28 15:47:12 +0300

    Changing the fileIsopen state on div icon click

--- src/Folder.js
+++ src/Folder.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Icon from './Icon';
 
 function Folder({files}) {
+     const [fileOpen, setFileOpen ] =  useState(false);
 
     const findFileExtensionName = (fileName) => {
         const arr = fileName.split('.');
@@ -10,14 +11,44 @@ function Folder({files}) {
 
 
   return (
-    <div style={{display: 'flex' , alignItems: "center"}}>
+    <div>
+         <div style={{
+          color: '#fff',
+          cursor: 'pointer',
+          padding: '2px 4px',
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          borderRadius: '5px',
+        }} 
+        onClick={() => setFileOpen(!fileOpen)}
+        >
         {!files.isFolder  &&
-        <>
-        <Icon name='js'   style={{ height: '19px', marginRight: '8px', flexShrink: 0 }} />
-         <p>{files.name}</p> 
-        </>
+         <Icon name={findFileExtensionName(files.name)}   style={{ height: '19px', marginRight: '8px', flexShrink: 0 }} />
          }
-        
+
+        {files.isFolder ? (
+                <>
+                    {fileOpen ? (
+                    <Icon
+                        name="caretDown"
+                        style={{ height: '12px', marginRight: '4px', flexShrink: 0 }}
+                    />
+                    ) : (
+                    <Icon
+                        name="caretRight"
+                        style={{ height: '12px', marginRight: '4px', flexShrink: 0 }}
+                    />
+                    )}
+                </>
+                ) : null}
+            {files.name}    
+                
+
+        </div>
+   
+      
+       
        
     </div>
   )
