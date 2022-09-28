054c04852f8d6ec9c20001137106a57ffb053022
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-28 14:56:23 +0300

    Adding css for the bg and wrap container

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div >
-      
+    <div style={{ height: '100vh', backgroundColor: '#18181b' }} >
+       <div style={{
+            width: '20%',
+            backgroundColor: '#27272a',
+            height: '100%',
+            overflowX: 'scroll',
+            padding: '10px',
+          }}>
+            
+
+       </div>
     </div>
   );
 }
