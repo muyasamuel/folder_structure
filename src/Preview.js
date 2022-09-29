00cd657ca1903b8d3585d6a66707a2356be67904
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-29 13:07:24 +0300

    Install qss to encode for stringifying a query object

--- src/Preview.js
+++ src/Preview.js
@@ -1,17 +1,39 @@
 import React, { useState } from "react";
-import styles from './Preview.module.css'
+import styles from "./Preview.module.css";
+import { encode } from "qss";
 
 function Preview({ url, children }) {
-  const[showImage, setShowImage] = useState(false)
+  const width = 200;
+  const height = 125;
+
+  const param = encode({
+    url,
+    screenshot: true,
+    meta: false,
+    embed: "screenshot.url",
+    colorScheme: "dark",
+    "viewport.isMobile": true,
+    "viewport.deviceScaleFactor": 1,
+    "viewport.width": width * 3,
+    "viewport.height": height * 3,
+  });
+
+  
+  const [showImage, setShowImage] = useState(false);
 
   return (
     <div className={styles.wrapper}>
-        {
-            showImage &&   <div className={styles.imageDiv}> hello hello am here</div>
-        }
-     
+      {showImage && <div className={styles.imageDiv}> hello hello am here</div>}
 
-      <a href={url} onMouseEnter={() => setShowImage(true)} onMouseLeave={() => setShowImage(false)} > {children}</a>
+      <a
+        href={url}
+        target="__blank"
+        onMouseEnter={() => setShowImage(true)}
+        onMouseLeave={() => setShowImage(false)}
+      >
+        {" "}
+        {children}
+      </a>
     </div>
   );
 }
